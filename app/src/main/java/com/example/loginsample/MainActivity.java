0b72c524d1712b2f51b84d72db4713018a45ab33
Gustavo Aguilar <gaguilarf@unsa.edu.pe>
2024-09-16 12:06:24 -0500

    Version 1.0

--- app/src/main/java/com/example/loginsample/MainActivity.java
+++ app/src/main/java/com/example/loginsample/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.loginsample;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.loginsample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        /*EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText editUsername = binding.editUsername;
+        EditText editPassword = binding.editPassword;
+        Button btnLogin = binding.btnLogin;
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editUsername.getText().toString().equals("admin") && editPassword.getText().toString().equals("admin")){
+                    Toast.makeText(getApplicationContext(), "Bienvenido a mi app",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Bienvenido a mi app");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error en la autenticacion",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Error en la autenticacion");
+                }
+            }
         });
+
     }
 }
