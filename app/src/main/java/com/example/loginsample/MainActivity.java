34801e866271c02ebdb4f30a3397678db9451f64
Gustavo Aguilar <gaguilarf@unsa.edu.pe>
2024-09-30 11:01:08 -0500

    version 1.1

--- app/src/main/java/com/example/loginsample/MainActivity.java
+++ app/src/main/java/com/example/loginsample/MainActivity.java
@@ -23,13 +23,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
@@ -39,16 +32,13 @@ public class MainActivity extends AppCompatActivity {
         EditText editPassword = binding.editPassword;
         Button btnLogin = binding.btnLogin;
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(editUsername.getText().toString().equals("admin") && editPassword.getText().toString().equals("admin")){
-                    Toast.makeText(getApplicationContext(), "Bienvenido a mi app",Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Bienvenido a mi app");
-                } else {
-                    Toast.makeText(getApplicationContext(), "Error en la autenticacion",Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Error en la autenticacion");
-                }
+        btnLogin.setOnClickListener(view1 -> {
+            if(editUsername.getText().toString().equals("admin") && editPassword.getText().toString().equals("admin")){
+                Toast.makeText(getApplicationContext(), "Bienvenido a mi app",Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Bienvenido a mi app");
+            } else {
+                Toast.makeText(getApplicationContext(), "Error en la autenticacion",Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Error en la autenticacion");
             }
         });
 
